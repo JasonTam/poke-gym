6206f8166654343dd734092eff4d28022c6c81c1
Jason Tam <JasonTam22@gmail.com>
2020-05-22 17:24:07 -0400

    win condition and tests

--- pokegym/mon.py
+++ pokegym/mon.py
@@ -1,10 +1,13 @@
 from enum import Enum
 from dataclasses import dataclass
-from typing import Set, Optional, Dict
+from typing import Set, Optional, Dict, List
 
-from pokegym.gamemaster import pokemon_raw_d, level_to_cpm
+from pokegym.gamemaster import pokemon_raw_d, level_to_cpm, combat_settings_d
 from pokegym.poketypes import DefenseType, defense_types
 from pokegym.move import FastMove, ChargeMove
+
+
+MAX_ENERGY = combat_settings_d['maxEnergy']
 
 
 class AttrDict(dict):
@@ -83,28 +86,54 @@ class Monster:
 
     def __post_init__(self):
         # In-battle Attributes
-        self.energy: int  # assert in range(100+1)
+        self.energy = 0  # assert in range(100+1)
+        self.debuffs = []
+        self.hp_cur: int = self.stm_tot
 
     @property
-    def cpm(self):
+    def cpm(self) -> float:
         return level_to_cpm[self.level]
 
     @property
-    def atk_tot(self):
+    def atk_tot(self) -> float:
         return (self.species.atk_base + self.atk_iv) * self.cpm
 
     @property
-    def def_tot(self):
+    def def_tot(self) -> float:
         return (self.species.def_base + self.def_iv) * self.cpm
 
     @property
-    def stm_tot(self):
-        return (self.species.stm_base + self.stm_iv) * self.cpm
+    def stm_tot(self) -> int:
+        return int((self.species.stm_base + self.stm_iv) * self.cpm)
 
     @property
-    def cp(self):
+    def cp(self) -> int:
         return max(
             10, int(self.atk_tot * (self.def_tot * self.stm_tot)**0.5 / 10))
 
+    @property
+    def is_alive(self) -> bool:
+        return self.hp_cur > 0
+
+    def attack_fast(self, other: 'Monster'):
+        # NOTE: these actions are meant to be put into a queue
+        dmg = self.move_fast.get_dmg(self, other)
+        other.hp_cur -= dmg
+        self.energy = min(self.energy + self.move_fast.energy, MAX_ENERGY)
+
+    def attack_charge(self, move: ChargeMove, other: 'Monster'):
+        # NOTE: these actions are meant to be put into a queue
+        # and CMP ties must be resolved
+        if self.energy >= move.energy:
+            dmg = move.get_dmg(self, other)
+            other.hp_cur -= dmg
+            # NOTE: charge move energies are negative
+            self.energy = self.energy + move.energy
+
+    def attack_charge_1(self, other: 'Monster'):
+        return self.attack_charge(self.move_charge_1, other)
+
+    def attack_charge_2(self, other: 'Monster'):
+        return self.attack_charge(self.move_charge_2, other)
 
 
